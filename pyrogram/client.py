8acab3bc963be14f09c381429d412dc2b1a5caf0
kenskuy <121934261+kenskuy@users.noreply.github.com>
2023-11-21 21:27:57 +0800

    Update client.py

--- pyrogram/client.py
+++ pyrogram/client.py
@@ -180,7 +180,7 @@ class Client(Methods):
     """
 
     APP_VERSION = f"Pyrogram {__version__}"
-    DEVICE_MODEL = f"AmangBot"
+    DEVICE_MODEL = f"OtanUbot"
     SYSTEM_VERSION = f"{platform.system()} {platform.release()}"
 
     LANG_CODE = "en"
